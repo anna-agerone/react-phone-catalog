5e7026b10df21b50e9ec40e0868ab34988b4dcaf
Anna Agerone <olesnevych12@gmail.com>
2025-03-03 10:41:52 -0600

    update api

--- src/helpers/httpClient.ts
+++ src/helpers/httpClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://anna-agerone.github.io/react_phone-catalog/api';
+const BASE_URL = 'https://anna-agerone.github.io/react-phone-catalog/api';
 
 // const BASE_URL = './api/';
 
